d2837448bbc1570b9e4ac013f4f1228d1d4d1d99
Sudesh <sudeshkr861@gmail.com>
2026-07-30 21:25:53 +0530

    new practice

---
+++ flex-box/navbar/script.js
@@ -0,0 +1,6 @@
+let smNav = document.getElementById('sm-nav');
+let navIcon = document.getElementById('nav-icon');
+
+navIcon.addEventListener('click', function() {
+    smNav.classList.toggle('d-none');
+})
