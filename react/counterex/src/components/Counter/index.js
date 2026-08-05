d15e6d9693eb6d8b9569b9c17570a51085bddcda
Sudesh <sudeshkr861@gmail.com>
2026-08-05 18:17:21 +0530

    dynamic counter

--- react/counterex/src/components/Counter/index.js
+++ react/counterex/src/components/Counter/index.js
@@ -2,14 +2,33 @@ import {Component} from 'react'
 import './index.css'
 
 class Counter extends Component {
+  state = {count: 0}
+  onIncrement = () => {
+    this.setState(prevState => {
+      return {count: prevState.count + 1}
+    })
+  }
+
+  onDecrement = () => {
+    this.setState(prevState => {
+      return {count: prevState.count - 1}
+    })
+  }
+
   render() {
+    const {count} = this.state
+
     return (
       <div className="container">
         <h1 className="heading">Counter</h1>
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <div>
-          <button className="button">Increase</button>
-          <button className="button">Decrease</button>
+          <button onClick={this.onIncrement} className="button">
+            Increase
+          </button>
+          <button onClick={this.onDecrement} className="button">
+            Decrease
+          </button>
         </div>
       </div>
     )
